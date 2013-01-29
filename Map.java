b8d44abe6e3ee1b4fd7db575e8d0ff0b57db805a
xacobe97 <xacobe97dev@gmail.com>
2013-01-29 19:21:13 +0100

    optimized map generator

--- Map.java
+++ Map.java
@@ -2,38 +2,101 @@ import java.util.Random;
 
 public class Map {
 
-	int coordenadas[][] = new int[64][64];
+String coordenadas[][] = new String[50][50];
+int ii = 0;
+int i = 0;
+int valor = 0;
+Random r = new Random();
+int a, b, randa, randb, map;
+int counta = 0;
+String letter;
+int countb = 0;
 
-	public void crearmapa() {	
-		Random rand = new Random();
-		int i, ii;
+public void setMapa(int ii, int i, String valor) {
 
-		for (ii = 0; ii < 10; ii++) {
-			for (i = 0; i < 10; i++) {
+	this.ii = ii;
+	this.i = i;
+	
+	coordenadas[ii][i] = valor;
+	}
 
-				int o = rand.nextInt( 2 - 1 + 1 );
-				coordenadas[ii][i] = o;
-				System.out.print(coordenadas[ii][i]);
+public void createMap() {
+	
+	i = 0;
+	while (i < 3) {
+		
+		map = r.nextInt(10 - 1 + 1);
+	
+		switch (map) {
+		case 1:
+			waterBiome();
+		case 2:
+			waterBiome();
+		case 3:
+			waterBiome();
+		case 4:
+			waterBiome();
+		case 5:
+			waterBiome();
+		case 6:
+			waterBiome();
+		case 7:
+			desertBiome();
+		case 8:
+			desertBiome();
+		case 9:
+			jungleBiome();
+		case 10:
+			jungleBiome();
+		}
+		i++;
+	}
+}
 
+public void waterBiome() {
+	
+	letter = "W";
+	randa = r.nextInt( 20 - 1 + 1 );
+	randb = r.nextInt( 20 - 1 + 1 );
+	
+	for (a = 0; a < randa; a++) {
+		for (b = 0; b < randb; b++) {
+			coordenadas[a][b] = letter;
+			System.out.print(coordenadas[a][b]);
 			}
-			
-			System.out.println("");
-			
+		System.out.println("");
 		}
 	}
+
+public void desertBiome() {
 	
-	int ii = 0;
-	int i = 0;
-	int valor = 0;
-
-	public void setMapa(int ii, int i, int valor) {
-
-		this.ii = ii;
-		this.i = i;
-		this.valor = valor;
-
-		coordenadas[ii][i] = valor;
-
+	letter = "D";
+	randa = r.nextInt( 15 - 1+ 1 );
+	randb = r.nextInt( 15 - 1 + 1 );
+	
+	for (a = 0; a < randa; a++) {
+		for (b = 0; b < randb; b++) {
+			coordenadas[a][b] = letter;
+			System.out.print(coordenadas[a][b]);
+			}
+		System.out.println("");
+		}
 	}
+
+public void jungleBiome() {
+	
+	letter = "J";
+	randa = r.nextInt( 5 - 1+ 1 );
+	randb = r.nextInt( 5 - 1 + 1 );
+	
+	for (a = 0; a < randa; a++) {
+		for (b = 0; b < randb; b++) {
+			coordenadas[a][b] = letter;
+			System.out.print(coordenadas[a][b]);
+			}
+		System.out.println("");
+		}
+	}
+
 }
 
--- Player.java
+++ Player.java
@@ -1,6 +1,22 @@
 public class Player {
 	
 	int hp = 1000;
+	int[][][] inv = new int[32][64][100];
 	
+	int pos = 0;
+	int num = 0;
+	int id = 0;
+	
+	public void setInv(int pos, int num, int id) {
+		
+		// pos = inventory position
+		// num = number of items (only same item)
+		// id = item ID
+		
+		this.pos = pos;
+		this.num = num;
+		this.id = id;
+		
+	}
 	
 }
